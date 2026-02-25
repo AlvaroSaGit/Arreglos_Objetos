cc8ff0e0c299e008fc423e5a18e5a7ce7105baad
Alvaro <salazares2005@gmail.com>
2026-02-25 18:28:48 -0500

    Se ha desarrollado el punto 10, en la carpeta de desarrollo estaran todos los puntos desarrollados del mismo 10, en este directorio esta el index.js para usarlo como barril para usarlo en app.js

--- actividad_transferencia/ejercicio_10/desarrollo/index.js
+++ actividad_transferencia/ejercicio_10/desarrollo/index.js
@@ -1,4 +1,4 @@
 export {puntoA} from "./puntoA.js";
 export {puntoB} from "./puntoB.js";
-export {} from "./puntoC.js";
-export {} from "./puntoD.js";
+export {puntoC} from "./puntoC.js";
+export {puntoD} from "./puntoD.js";
