a00d0800fa4d3f03663df5d4f3ef00d8c0ea69e0
Alvaro <salazares2005@gmail.com>
2026-02-25 19:03:33 -0500

    Se ha desarrollado el objeto libro, donde se imprimen todos sus atributos llamando al propio objeto y con la puntuacion indicando los atributos

---
+++ actividad_transferencia/ejercicio_8/desarrollo/arreglo.js
@@ -0,0 +1,12 @@
+export function trabajo(){
+    // Objeto libro con sus propiedades
+    const libro ={
+        titulo: "Cien años de soledad",
+        autor: "Gabriel Garcia Marquez",
+        paginas: 471,
+        genero: "Realismo Mágico"
+    }
+    // Imprimir el mensaje con los atributos que tiene el objeto libro
+    console.log(`El libro: ${libro.titulo}\nEl autor: ${libro.autor}\nPaginas: ${libro.paginas}\nGenero: ${libro.genero}`);
+    
+}
